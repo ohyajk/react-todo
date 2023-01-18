f86643510acc9100972c5a29161a913387aaa77c
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2023-01-18 11:17:21 +0530

    Update Index for class

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom"
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-//component
-import TodoContainer from "./functionBased/components/TodoContainer"
+import TodoContainer from "./classBased/components/TodoContainer"
 
-//stylesheet
-import "./functionBased/App.css"
+import "./classBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
